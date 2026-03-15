e946673155346af87e86e7bc6af77b71868a4f0d
dscrtdv <ti.discretedev@gmail.com>
2026-03-15 23:50:22 +0100

    -Computer movable + power on off

--- Computer/computer.gd
+++ Computer/computer.gd
@@ -1,23 +1,52 @@
 extends Sprite2D
 
-var mouse_over = false
+@onready var screen     := $Screen
 
-func _ready():
-    pass
+var mouse_over          := false
+var grabbing            := false
+var powered             := false
+var mousePos : Vector2  = Vector2.ZERO
+var deltaPos : Vector2  = Vector2.ZERO
 
-func _input(event):
-    if mouse_over and event.is_action_pressed("mouse_click"):
-        print("Computer clicked")
-        
+var screen_on_color : Color = Color(0.192, 0.718, 0.643)
+var screen_off_color : Color = Color(0.2, 0.2, 0.2) # Dark gray
 
-func _process(delta):
-    pass
+var hover_scale := Vector2(1.05, 1.05)
+var normal_scale := Vector2(1.0, 1.0)
+
+func _ready() -> void:
+	screen.modulate = screen_off_color
+
+func _process(_delta: float) -> void:
+	deltaPos = mousePos - get_global_mouse_position()
+	mousePos = get_global_mouse_position()
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("left_click") and mouse_over or grabbing:
+		global_position -= deltaPos
+		grabbing = true
+	if Input.is_action_just_released("left_click"):
+		if grabbing:
+			grabbing = false
+	if Input.is_action_just_pressed("right_click") and mouse_over:
+		powered = not powered
+		print("[!]Computer state changed ", powered)
+		if powered:
+			print("[!]Computer powered on")
+			screen.modulate = screen_on_color
+		else:
+			print("[!]Computer powered off")
+			screen.modulate = screen_off_color
 
 func _on_mouse_entered() -> void:
-    print("Mouse entered computer")
-    mouse_over = true  
-
+	mouse_over = true
+	print("[+]Mouse IN")
+	var tween = create_tween()
+	tween.tween_property(self, "scale", hover_scale, 0.15)
 
 func _on_mouse_exited() -> void:
-    print("Mouse exited computer")
-    mouse_over = false
+	if not grabbing:
+		mouse_over = false
+		print("[-]Mouse OUT")
+		var tween = create_tween()
+		tween.tween_property(self, "scale", normal_scale, 0.15)
